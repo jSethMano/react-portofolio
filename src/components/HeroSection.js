a82c55260a191532f016a4e09bc95aad57f53851
jSethMano <joshuaseth11@gmail.com>
2022-09-13 08:44:31 +0800

    added-useEffect

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import image from "../assets/hero-section/self-image.png";
 import ButtonWrapper from "./wrapper/ButtonWrapper";
 
